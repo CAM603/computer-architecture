cafdbdb94a342a112ba55feabbc0282ac24a2bba
CAM603 <cameronhawley@comcast.net>
2020-06-15 17:37:26 -0400

    add day1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,19 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256  # 256 bytes of memory
+        self.reg = [0] * 8  # 8 general purpose registers
+        self.pc = 0  # program counter, index of the current instruction
+        # Instruction definitions
+        self.HLT = 0b00000001
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +25,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +54,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +68,29 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+
+        while running:
+            ir = self.ram[self.pc]  # Instruction Register
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if ir == self.HLT:  # end the loop
+                running = False
+            elif ir == self.LDI:  # sets a specified register to a specified value
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif ir == self.PRN:  # print numeric value stored in the given register.
+                print(self.reg[operand_a])
+                self.pc += 2
+            else:
+                print(f'Unknown instruction {ir} at address {self.pc}')
+                sys.exit(1)
+
+    def ram_read(self, mar):
+        # MAR: Memory Address Register contains the address that is being read or written to
+        return self.ram[mar]
+
+    def ram_write(self, mdr, address):
+        # MDR: Memory Data Register contains the data that was read or the data to write
+        self.ram[address] = mdr
